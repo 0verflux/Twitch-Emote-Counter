38bd4f9b9d227cefac1a20697d5b34a5ed82ad09
Gedeon <francisdemin@hotmail.com>
2022-11-04 02:19:32 +0800

    removed ConsoleApp2

--- ConsoleApp2/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿var channel = args[0];
-var username = args[1];
-
-using var client = new HttpClient();
-
-client.DefaultRequestHeaders.Accept.Clear();
-client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/http"));
